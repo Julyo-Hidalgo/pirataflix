26cbd5f231b2cb8d6294bfa62596088e853b2bb1
Julyo <julyohidalgo0@gmail.com>
2022-06-21 23:43:10 -0300

    first commit

---
+++ pirataflix/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pirataflix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            logo.Source = ImageSource.FromResource("pirataflix.img.logo.png");
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new categoria.marvel());
+            }catch (Exception ex)
+            {
+                await DisplayAlert("Ops, algo deu errado...", ex.Message.ToString(), "Ok");
+            }
+        }
+    }
+}
---
+++ pirataflix/categoria/marvel.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pirataflix.categoria
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class marvel : ContentPage
+    {
+        public marvel()
+        {
+            InitializeComponent();
+            logo.Source = ImageSource.FromResource("pirataflix.img.logo.png");
+            avengers_eg.Source = ImageSource.FromResource("");
+            capitaoamerica_gc.Source = ImageSource.FromResource("pirataflix.poster.capitaoamerica_gc.jpg");
+            ironman3.Source = ImageSource.FromResource("pirataflix.poster.ironman3.jpg");
+            spidermannwh.Source = ImageSource.FromResource("pirataflix.poster.spidermannwh.jpg");
+        }
+    }
+}
